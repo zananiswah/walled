fcd65c0fe014d0efe2f72694b8e75c4305e0d8a9
Zana <zana.niswah@gmail.com>
2024-12-09 13:42:55 +0700

    feat: Impleemnt state for active tab

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import logo from "./assets/logo.png";
 import NavItems from "./components/NavItems";
 
 function App() {
+  const [activeTab, setActiveTab] = useState("Dashboard");
   const menu = [
     {
       title: "Dashboard",
@@ -20,12 +22,17 @@ function App() {
       link: "#",
     },
   ];
+
   return (
     <>
       <nav className="flex items-center w-screen justify-between bg-white py-4 px-8">
         <img src={logo} />
         <div className="flex gap-x-8">
-          <NavItems menu={menu} />
+          <NavItems
+            menu={menu}
+            activeTab={activeTab}
+            handleClick={setActiveTab}
+          />
         </div>
       </nav>
     </>
--- src/components/NavItems.jsx
+++ src/components/NavItems.jsx
@@ -1,8 +1,15 @@
-function NavItems({ menu }) {
+function NavItems({ menu, activeTab, handleClick }) {
   return (
     <div className="flex gap-x-8">
       {menu.map((item, index) => (
-        <a key={index} href={item.link}>
+        <a
+          onClick={() => handleClick(item.title)}
+          className={
+            activeTab !== item.title ? "text-black" : "text[#19918F] font-bold"
+          }
+          key={index}
+          href={item.link}
+        >
           {item.title}
         </a>
       ))}
